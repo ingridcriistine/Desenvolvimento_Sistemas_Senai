4a5b3ef83d37db744686bb8fb42d4184df315e36
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2025-02-07 17:10:38 -0300

    Create oficina.js

---
+++ Testes/oficina.js
@@ -0,0 +1,33 @@
+use ('oficina');
+
+db.cliente.insert([
+    {
+        nome: "Ingrid",
+        telefone: "41999995555",
+        email: "ingrid@email.com",
+        endereço: "R. lalalala"
+    },
+])
+
+db.veiculo.insert([
+    {
+        nome: "",
+        marca: "",
+        placa: "",
+        ano: ""
+    },
+])
+
+db.servico.insert([
+    {
+        nome: "",
+        preço: ""
+    },
+])
+
+db.agendamento.insert([
+    {
+        data: "",
+        status: ""
+    },
+])
